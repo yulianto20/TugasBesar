47551c8e71abe21b4265e9e233c8e671741f59f4
WahyudinScatt <Wahyudin.1040132@gmail.com>
2013-12-02 20:51:29 +0700

    commit pertama

---
+++ src/test/Test.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test;
+
+/**
+ *
+ * @author WahyudinScatt
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("test");
+    // TODO code application logic here
+    }
+    
+}
