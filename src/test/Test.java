70ed13393ea763722f8fa207f26d1f5887963709
WahyudinScatt <Wahyudin.1040132@gmail.com>
2013-12-02 21:03:02 +0700

    tambah sysyot

--- src/test/Test.java
+++ src/test/Test.java
@@ -17,6 +17,7 @@ public class Test {
      */
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("yxgsyigx");
     // TODO code application logic here
     }
     
